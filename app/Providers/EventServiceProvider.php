b2c5ab88a20a6f857161653970661d62c276e1ce
vagrant <vagrant@homestead>
2018-12-18 03:50:21 +0000

    订单模块调整

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,14 +2,15 @@
 
 namespace App\Providers;
 
-use App\Events\OrderReviewd;
-use App\Listeners\UpdateProductRating;
 use App\Events\OrderPaid;
-use App\Listeners\UpdateProductSoldCount;
+use App\Events\OrderReviewd;
 use App\Listeners\SendOrderPaidMail;
+use Illuminate\Support\Facades\Event;
 use App\Listeners\RegisteredListener;
 use Illuminate\Auth\Events\Registered;
-use Illuminate\Support\Facades\Event;
+use App\Listeners\UpdateProductRating;
+use App\Listeners\UpdateProductSoldCount;
+use App\Listeners\UpdateCrowdfundingProductProgress;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
@@ -26,6 +27,7 @@ class EventServiceProvider extends ServiceProvider
         OrderPaid::class => [
             UpdateProductSoldCount::class,
             SendOrderPaidMail::class,
+            UpdateCrowdfundingProductProgress::class,
         ],
         OrderReviewd::class => [
             UpdateProductRating::class,
